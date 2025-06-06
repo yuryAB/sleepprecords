4ecfef8a621015d0ac19ddf61ee17f72cfd3abea
YuryAB <yury.antonyb@gmail.com>
2025-06-06 18:48:55 -0400

    Add optional sleepStart, paralysisDuration, SleepMetrics, and RoutineMetrics to Record model.

---
+++ REM/MVVM/Model/RoutineMetrics.swift
@@ -0,0 +1,16 @@
+//
+//  RoutineMetrics.swift
+//  REM
+//
+//  Created by yury antony on 06/06/25.
+//
+
+import Foundation
+
+struct RoutineMetrics {
+    var stressLevel: Int?
+    var caffeineConsumption: Int?
+    var alcoholConsumption: Int?
+    var physicalActivityLevel: Int?
+    var screenUseLevel: Int?
+}
---
+++ REM/MVVM/Model/SleepMetrics.swift
@@ -0,0 +1,15 @@
+//
+//  SleepMetrics.swift
+//  REM
+//
+//  Created by yury antony on 06/06/25.
+//
+
+import Foundation
+
+struct SleepMetrics {
+    var sleepQuality: Int?
+    var noiseLevel: Int?
+    var lightLevel: Int?
+    var temperatureLevel: Int?
+}
