f711e11173e940ca6432a69d769e16e283cc47ef
YuryAB <yury.antonyb@gmail.com>
2025-06-07 11:44:57 -0400

    Implemented  RoutineMetrics and SleepMetrics on Record model

--- REM/MVVM/Model/RoutineMetrics.swift
+++ REM/MVVM/Model/RoutineMetrics.swift
@@ -6,11 +6,32 @@
 //
 
 import Foundation
+import SwiftData
 
-struct RoutineMetrics {
+@Model
+final class RoutineMetrics {
+    @Attribute(.unique) var id: UUID
+    var record: Record?
+
     var stressLevel: Int?
     var caffeineConsumption: Int?
     var alcoholConsumption: Int?
     var physicalActivityLevel: Int?
     var screenUseLevel: Int?
+
+    init(
+        id: UUID = UUID(),
+        stressLevel: Int? = nil,
+        caffeineConsumption: Int? = nil,
+        alcoholConsumption: Int? = nil,
+        physicalActivityLevel: Int? = nil,
+        screenUseLevel: Int? = nil
+    ) {
+        self.id = id
+        self.stressLevel = stressLevel
+        self.caffeineConsumption = caffeineConsumption
+        self.alcoholConsumption = alcoholConsumption
+        self.physicalActivityLevel = physicalActivityLevel
+        self.screenUseLevel = screenUseLevel
+    }
 }
--- REM/MVVM/Model/SleepMetrics.swift
+++ REM/MVVM/Model/SleepMetrics.swift
@@ -6,10 +6,29 @@
 //
 
 import Foundation
+import SwiftData
 
-struct SleepMetrics {
+@Model
+final class SleepMetrics {
+    @Attribute(.unique) var id: UUID
+    var record: Record?
+
     var sleepQuality: Int?
     var noiseLevel: Int?
     var lightLevel: Int?
     var temperatureLevel: Int?
+
+    init(
+        id: UUID = UUID(),
+        sleepQuality: Int? = nil,
+        noiseLevel: Int? = nil,
+        lightLevel: Int? = nil,
+        temperatureLevel: Int? = nil
+    ) {
+        self.id = id
+        self.sleepQuality = sleepQuality
+        self.noiseLevel = noiseLevel
+        self.lightLevel = lightLevel
+        self.temperatureLevel = temperatureLevel
+    }
 }
